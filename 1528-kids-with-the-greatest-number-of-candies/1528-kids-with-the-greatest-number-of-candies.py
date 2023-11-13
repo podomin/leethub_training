e1b7a6c4d24ae05e45febd0e69c5d1c1ac1b2f73
podomin <82488911+podomin@users.noreply.github.com>
2023-11-13 09:56:55 +0900

    Time: 43 ms (64.47%), Space: 16.3 MB (36.37%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        # 1. 현재 candies에서 가장 큰 값 찾기
+        # 2. for 문을 돌면서 현재 candy 개수와 extraCandies 합쳐주기
+        # 3. 합친 값이 가장 큰 값보다 크다면 True, 아니면 False를 answer에 추가하기
+        answer = []
+        max_candies = max(candies)
+        for candy in candies:
+            added_candy = candy + extraCandies
+            # print(added_candy, max_candies)
+            if added_candy >= max_candies:
+                answer.append(True)
+            else: 
+                answer.append(False)           
+        return answer
+
+        
