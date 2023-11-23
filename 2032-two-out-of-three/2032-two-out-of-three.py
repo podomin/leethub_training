c524ae209e5a7983ebcc4e3fada3a206ca0e034e
podomin <82488911+podomin@users.noreply.github.com>
2023-11-23 09:44:53 +0900

    Time: 79 ms (23.32%), Space: 16.5 MB (23.22%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -3,8 +3,6 @@ class Solution:
         set1 = set(nums1)
         set2 = set(nums2)
         set3 = set(nums3)
-        print(set1, set2, set3)
-        print((set1 & set2) | (set2 & set3) | (set1 & set3))
         answer = list((set1 & set2) | (set2 & set3) | (set1 & set3))
 
         return answer
