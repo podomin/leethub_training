55f5b5d39854e670cb845c8dd2075a5e350679bc
podomin <82488911+podomin@users.noreply.github.com>
2024-01-18 10:05:21 +0900

    Time: 37 ms (66.75%), Space: 17.4 MB (25.94%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -5,10 +5,8 @@ class Solution:
         for i in range(len(nums) - k + 1):
             int_n = nums[i:i+k]
             print(int_n)
-            if int(int_n) == 0:
-                pass
-            elif num % int(int_n) == 0:
+            if int(int_n) != 0 and num % int(int_n) == 0:
                 answer += 1
-             
+   
         return answer
         
