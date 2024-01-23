850d83d46b33080511be68194f4dd36659c31c67
podomin <82488911+podomin@users.noreply.github.com>
2024-01-23 10:06:20 +0900

    Time: 46 ms (48.58%), Space: 16.7 MB (69.24%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        answer = []
+        if k > 0:
+            for i in range(len(code)):
+                cur_sum = 0
+                for j in range(k):
+                    #print(code[i], code[(i+j+1) % len(code)])
+                    cur_sum += code[(i+j+1) % len(code)]
+                answer.append(cur_sum)
+        elif k < 0:
+            for i in range(len(code)):
+                cur_sum = 0
+                for j in range(-k):
+                    #print(code[i], code[(i-j-1)])
+                    cur_sum += code[(i-j-1)]
+                answer.append(cur_sum)
+        elif k == 0:
+            answer = [0] * len(code)
+        return answer
+        
